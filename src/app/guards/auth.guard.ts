76b75b43c65f3afd99445b16a07bdfe2092aac6e
Juan Andres Frigo <juan.a.frigo@gmail.com>
2023-03-19 20:43:32 -0300

    Fin seccion 19

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,17 +2,29 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanLoad,
+  Route,
   Router,
   RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
 } from '@angular/router';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { UsuarioService } from '../services/usuario.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private usuarioService: UsuarioService, private router: Router) {}
+
+  canLoad(route: Route, segments: UrlSegment[]) {
+    return this.usuarioService.validarToken().pipe(
+      tap((estaAutenticado: boolean) => {
+        if (!estaAutenticado) this.router.navigateByUrl('/login');
+      })
+    );
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.usuarioService.validarToken().pipe(
